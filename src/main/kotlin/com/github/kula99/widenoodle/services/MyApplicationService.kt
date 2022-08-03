7010aec4248f1d857d92f517e63b03db91c4502b
GitHub Action <action@github.com>
2022-08-03 03:25:38 +0000

    Template cleanup

--- src/main/kotlin/com/github/kula99/widenoodle/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/kula99/widenoodle/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.kula99.widenoodle.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kula99.widenoodle.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/kula99/widenoodle/services/MyProjectService.kt
+++ src/main/kotlin/com/github/kula99/widenoodle/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.kula99.widenoodle.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kula99.widenoodle.MyBundle
 
 class MyProjectService(project: Project) {
 
